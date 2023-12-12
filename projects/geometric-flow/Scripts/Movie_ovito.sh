4ceb767539b791a44eff12e472adda6e8cff21da
Mau <v.mauriciorojas@gmail.com>
2023-12-12 09:56:07 +0100

    Read_E modifcations

--- projects/geometric-flow/Scripts/Movie_ovito.sh
+++ projects/geometric-flow/Scripts/Movie_ovito.sh
@@ -30,7 +30,7 @@ set Nsim=1
 
 # conda activate ovito
 
-for nu in 0.2 0.3 0.4 0.5 0.6 0.63 0.7 0.8 1.0 
+for nu in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 
 # for Strg in 0.002 0.6 
 # foreach Strg (  )
 do
--- projects/geometric-flow/Scripts/Python_analisis.sh
+++ projects/geometric-flow/Scripts/Python_analisis.sh
@@ -7,11 +7,12 @@ module load python/3.9.7
 EXPORT PATH=/nfs/scistore16/wojtgrp/mrojasve/.local/bin:$PATH
 
 #foreach v ( 0.1 0.3 0.58 0.6 0.65 0.75 0.8 1.0 )
-foreach v ( 0.2 0.3 0.4 0.59 0.61 0.63 0.65 0.8 1.0  )
-foreach KB ( 0.005)
+foreach v ( 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0  )
+# foreach KB ( 0.005)
 
-python3 Plot_data_cluster.py ${v} ${KB}
+python3 Read_E.py ${v}
+# python3 Plot_data_cluster.py ${v} ${KB}
 # python3 Seing_meshes.py
 
 end
-end
+# end
