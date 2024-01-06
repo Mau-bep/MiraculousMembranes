c34090c7ca70d9bb7f4083f62d1c09a5237aa60b
Mau <v.mauriciorojas@gmail.com>
2024-01-06 13:49:38 +0100

    Update Movie_ovito.sh

--- projects/geometric-flow/Scripts/Movie_ovito.sh
+++ projects/geometric-flow/Scripts/Movie_ovito.sh
@@ -36,62 +36,55 @@ set Nsim=1
 
 
 
-# set KB=0.005
-foreach KB ( 0.1 )
+set KB=0.1
 set Nsim=1
 
-foreach v ( 0.5 0.55 0.6 0.65 0.7 0.75 0.85 0.9 0.95 1.0 )
-foreach Init_cond ( 1 )
 
-python Movie_ovito.py ${nu} ${Init_cond} 1 ${KB}
-
-end
-end
-
-
-
-foreach v ( 0.45 0.5 0.55 0.6 0.65 0.7 0.75 )
-# foreach v ( 0.475 0.525 0.575 0.625 0.675 0.725 )
-
-foreach Init_cond ( 2 )
-
-python Movie_ovito.py ${nu} ${Init_cond} 1 ${KB}
-
-end
-end
-
-
-foreach v ( 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 )
-foreach Init_cond ( 3 )
-
-python Movie_ovito.py ${nu} ${Init_cond} 1 ${KB}
-
-end
-end
-
-
-
-
-
-
-
-end
-
-
-
-
-
-
-for nu in 0.1 0.4 0.6 0.7 1.0 
+for nu in 0.5 0.55 0.6 0.65 0.7 0.75 0.85 0.9 0.95 1.0
 do
-for KB in 0.01 0.05 0.1 0.02
-do
-for Init_cond in 1 2 3
+for Init_cond in 1 
 do 
-    python Movie_ovito.py ${nu} ${Init_cond} 1 ${KB}
+python Movie_ovito.py ${nu} ${Init_cond} 1 ${KB}
+
 done 
 done
+
+
+
+for nu in 0.45 0.5 0.55 0.6 0.65 0.7 0.75
+do
+for Init_cond in 2 
+do
+python Movie_ovito.py ${nu} ${Init_cond} 1 ${KB}
+
+done 
 done
+
+
+for nu 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 
+do
+for Init_cond in 3 
+do
+python Movie_ovito.py ${nu} ${Init_cond} 1 ${KB}
+
+done 
+done
+
+
+
+
+
+
+# for nu in 0.1 0.4 0.6 0.7 1.0 
+# do
+# for KB in 0.01 0.05 0.1 0.02
+# do
+# for Init_cond in 1 2 3
+# do 
+#     python Movie_ovito.py ${nu} ${Init_cond} 1 ${KB}
+# done 
+# done
+# done
 #deactivate
 
 # conda deactivate ovito
