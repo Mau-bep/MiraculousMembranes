f85fd1dd74dbc048c7eccfac52cefb53ff260fc3
Mau <v.mauriciorojas@gmail.com>
2024-01-08 10:39:04 +0100

    Update Read_E.py

--- projects/geometric-flow/Scripts/Read_E.py
+++ projects/geometric-flow/Scripts/Read_E.py
@@ -203,6 +203,7 @@ def main_shape(Ini_cond):
             break
         if(len(splitted_line)>13):
             line=Output_file.readline()
+            line=Output_file.readline()
             continue
         # Bar_Areas.append(float(splitted_line[1]))
         # time_evol.append(float(splitted_line[2]))
