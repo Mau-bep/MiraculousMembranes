cc596d45ae48878a3234da8058364398bd64ebe6
Mau <v.mauriciorojas@gmail.com>
2024-03-12 22:06:39 +0100

    Update Mem_bead_up.sh

--- projects/geometric-flow/Scripts/Mem_bead_up.sh
+++ projects/geometric-flow/Scripts/Mem_bead_up.sh
@@ -5,10 +5,10 @@
 
 # #   SARIConGPT143!
 set v=1.0
-set Nsim=13
+set Nsim=15
 set Init_cond=1
 # foreach Strg ( 5.0 1.0 0.1 20.0 0.05 0.0005 0.0001 0.00001 )
-foreach Strg ( 0.005 0.01 0.05 0.1 0.5 1.0 5.0 10.0 )
+foreach Strg ( 0.005 0.01 0.03 0.05 0.08 0.1 0.25 0.5 0.75 1.0 1.25 1.5 1.8 2.0 )
 
 #python3 Create_subjob.py ${v} ${c0} ${KA} ${KB}
 python3 Create_subjob_beads.py ${v} ${Strg} ${Init_cond} ${Nsim}
