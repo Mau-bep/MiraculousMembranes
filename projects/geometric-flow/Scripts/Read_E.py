c4fefcbdb09236bf8da81869060d5ea392268b30
Mau <v.mauriciorojas@gmail.com>
2024-04-03 11:14:48 +0200

    Update Read_E.py

--- projects/geometric-flow/Scripts/Read_E.py
+++ projects/geometric-flow/Scripts/Read_E.py
@@ -6,17 +6,23 @@ import sys
 
 
 
-nu=float(sys.argv[1])
+# nu=float(sys.argv[1])
+nu = 1.0
 # KB=float(sys.argv[2])
 # strg=float(sys.argv[3])
 
 # nu=1.0
-Initial_conds=int(sys.argv[2])
+# Initial_conds=int(sys.argv[2])
+Initial_conds= 1
 # KB=float(sys.argv[3])
 KB=0.01
-strength = float(sys.argv[3])
-Nsim=int(sys.argv[4])
-KA = float(sys.argv[5])
+# strength = float(sys.argv[3])
+# Nsim=int(sys.argv[4])
+
+# KA = float(sys.argv[5])
+
+
+
 # def main_bead(strg):
 
 #     pre_folder='../Results/Mem3DG_Cell_Shape/'
@@ -547,19 +553,147 @@ def main_bead(Ini_cond):
 main_bead(Initial_conds)
 # OK soo
 
-# pre_folder='../Results/Mem3DG_Cell_Shape/'
-# plt.axhline(nu,color='black',ls='dashed')
-# plt.plot(data_1[0],data_1[1],color=(255/256,51/256,109/256),label='Prolate')
-# plt.plot(data_2[0],data_2[1],color=(123/256,40/256,125/256),label='Oblate')
-# plt.plot(data_3[0],data_3[1],color=(255/256,166/256,48/256),label='Stomatocyte')
-
-# plt.axhline(data_1[1][-1],ls='dashed',color=(255/256,51/256,109/256))
-# plt.axhline(data_2[1][-1],ls='dashed',color=(123/256,40/256,125/256))
-# plt.axhline(data_3[1][-1],ls='dashed',color=(255/256,166/256,48/256))
-# plt.legend()
-# # plt.xlim(0,np.min([data_1[0][-1],data_2[0][-1]]))
-# plt.savefig(pre_folder+'Imgs/Reduced_vol_evol_nu_{}.jpg'.format(nu))
 
 
+
+
+def bead_attachment_Energy(Ini_cond):
+
+    pre_folder='../Results/Mem3DG_Bead_Reciprocal/'
+    # pre_folder='../Results/Mem3DG_Cell_Shape_KB_evol/'
+    dir='nu_{:.3f}_c0_0.000_KA_{:.3F}_KB_{:.6f}_strength_{:.6f}_Init_cond_{}_Nsim_{}'.format(nu,KA,KB,strength,Ini_cond,Nsim)
+        #   nu_1.000_c0_0.000_KA_10.000_KB_0.005000_init_cond_1_Nsim_2
+    # dir_2='nu_{:.3f}_c0_0.000_KA_10.000_KB_{:.6f}_init_cond_{}_Nsim_{}'.format(nu,KB,Ini,Nsim)
+    
+    folder_path= pre_folder+dir+'/'
+
+    # Bead_path=folder_path+"Bead_data.txt"
+    Output_path=folder_path+"Output_data.txt"
+    # file_xyz = open(folder_path+'Bead_data.xyz','w')
+
+    # Bead_file = open(Bead_path)
+    Output_file= open(Output_path)
+    # line = Output_file.readline()
+    r = 1.0
+
+    counter=0
+    frame=0
+    line=Output_file.readline()
+
+    targ_vol=0.0
+    targ_area=0.0
+
+
+    # Sim_data << V_bar<<" "<< A_bar<<" "<< time <<" "<< V<<" " << A<<" " << E_Vol << " " << E_Sur << " " << E_Ben <<" " << E_bead << " "<< grad_norm<<" " << backtrackstep<< " "<< Bead_1.Pos.x << " "<< Bead_1.Pos.y << " "<< Bead_1.Pos.z <<" \n";
+
+    time_evol=[]
+    Volumes=[]
+    Bar_Areas=[]
+    Areas=[]
+    E_vol=[]
+    E_sur=[]
+    E_ben=[]
+    E_bead=[]
+    grad_norm=[]
+    backtrackstep=[]
+    line=Output_file.readline()
+    counter=0
+    while line:
+        # frame=counter*1000
+        splitted_line=line.split(' ')
+        # print(len(splitted_line))
+        if(len(splitted_line)<11):
+            break
+        if(len(splitted_line)>13):
+            line=Output_file.readline()
+            line=Output_file.readline()
+            continue
+        Bar_Areas.append(float(splitted_line[1]))
+        time_evol.append(float(splitted_line[2]))
+        Volumes.append(float(splitted_line[3]))
+        Areas.append(float(splitted_line[4]))
+        E_vol.append(float(splitted_line[5]))
+        E_sur.append(float(splitted_line[6]))
+        E_ben.append(float(splitted_line[7]))
+        E_bead.append(float(splitted_line[8]))
+        grad_norm.append(float(splitted_line[9]))
+        backtrackstep.append(float(splitted_line[10]))
+        
+
+
+        # V_bar=float(splitted_line[0])
+        # A_bar=float(splitted_line[1])
+        
+
+        # i can do a safety check 
+        iftruecontinue=False
+        for k in range(8):
+            if(len(splitted_line[k].split('.'))>2):
+                iftruecontinue=True
+                # line=Output_file.readline()
+                # continue
+        if(iftruecontinue):
+            line=Output_file.readline()
+            continue
+        
+        Bar_Area=float(splitted_line[1])
+        time=float(splitted_line[2])
+        Volume=float(splitted_line[3])
+        Area=float(splitted_line[4])
+        E_vol1=float(splitted_line[5])
+        E_sur1=float(splitted_line[6])
+        E_ben1=float(splitted_line[7])
+        E_bead1=float(splitted_line[8])
+        V_bar=float(splitted_line[0])
+        A_bar=float(splitted_line[1])
+        
+        
+        
+        line=Output_file.readline()
+        counter+=1
+
+
+    
+
+
+    # Ok so we have the data and its time to plot !
+
+    os.makedirs(pre_folder+"Imgs/",exist_ok=True)
+    os.makedirs(pre_folder+"Data/",exist_ok=True)
+
+
+    E_data = open(pre_folder+"Data/Final_bead_energies_Nsim_{}.txt".format(strength,Nsim),"a+")
+
+    E_data.write("{} {} {} {} {} {} {} {} \n".format(nu,Initial_conds,Nsim,KA,strength,E_bead1,Area,Volume))
+    # E_data.write("{} {} {} {} {} {} {} {} {} \n".format(nu,Initial_conds,Nsim,E_vol[-1],E_sur[-1],E_ben[-1],Areas[-1],Volumes[-1]))
+
+    E_data.close()
+
+    Output_file.close()
+
+    return E_bead[-1]
+
+
+
+
+
+KAs = [10, 100, 200, 500, 1000]
+Energies=[]
+strengths = [0.001, 0.01, 0.1, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0, 3.5, 4.0] 
+pre_folder='../Results/Mem3DG_Bead_Reciprocal/'
+    # pre_folder='../Results/Mem3DG_Cell_Shape_KB_evol/'
+Nsim=100
+for i in range(len(KAs)):
+    KA=KAs[i]
+    En=[]
+    for j in range(len(strengths)):
+        strength=strengths[j]
+        En.append(bead_attachment_Energy(1))
+    Energies.append(En)
+    plt.plot(strengths,En,label='KA = {}'.format(KA))
+
+plt.savefig(pre_folder+"/Imgs/Bead_Energy_differen_KA.png",bbox_inches='tight')
+
+print(Energies)
 
 
