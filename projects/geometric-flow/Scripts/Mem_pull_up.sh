5ec10b02582f14bc0aa4df624fa063223c96a570
Mau <v.mauriciorojas@gmail.com>
2024-04-09 13:36:24 +0200

    Stronger pulling force

--- projects/geometric-flow/Scripts/Mem_pull_up.sh
+++ projects/geometric-flow/Scripts/Mem_pull_up.sh
@@ -12,7 +12,7 @@ set Init_cond=1
 #  4.5 5.0 5.5 6.0 6.5 7.0 7.5 8.5 9.0 10.0
 # foreach Init_cond (1 3)
 foreach rc (2.0)
-foreach Strg ( 10.0 )
+foreach Strg ( 100.0 )
 foreach KB ( 0.01 0.04 0.09 0.16 0.25 0.36 0.49 0.64 0.81 1.00 )
 #python3 Create_subjob.py ${v} ${c0} ${KA} ${KB}
 python3 Create_subjob_beads_pull.py ${rc} ${Strg} ${Init_cond} ${Nsim} ${KB}
