58a77a2b8035c023766c4edc13293c798f549cc7
Mau <v.mauriciorojas@gmail.com>
2024-06-04 14:50:59 +0200

    Update Test_shape_up.sh

--- projects/geometric-flow/Scripts/Test_shape_up.sh
+++ projects/geometric-flow/Scripts/Test_shape_up.sh
@@ -12,7 +12,7 @@ foreach v ( 1.0 )
 
 python3 Create_subjob_test.py ${v} ${c0} ${KA} ${KB}
 
-sbatch subjob_test_th_memshape_v_${v}_c0_${c0}_KA_${KA}_KB_${KB}
+sbatch ../Subjobs/subjob_test_th_memshape_v_${v}_c0_${c0}_KA_${KA}_KB_${KB}
 
 end
 end
