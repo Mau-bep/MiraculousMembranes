2970ffc52c353acf70089a2a53c08dc5030cc314
Mau <v.mauriciorojas@gmail.com>
2024-06-04 14:48:53 +0200

    Update Test_shape_up.sh
    Solved minor detail with KB=0.0 instead of 0.00
    Also now it only calls for one v

--- projects/geometric-flow/Scripts/Test_shape_up.sh
+++ projects/geometric-flow/Scripts/Test_shape_up.sh
@@ -6,9 +6,9 @@
 set KA=10.0
 set c0=0.0
 foreach c0 (-12.0 12.0)
-foreach KB (0.00)
+foreach KB ( 0.0 )
 # foreach v ( 0.6 0.65 0.7 0.8 1.0 )
-foreach v (0.4 0.62 0.8 1.0 )
+foreach v ( 1.0 )
 
 python3 Create_subjob_test.py ${v} ${c0} ${KA} ${KB}
 
