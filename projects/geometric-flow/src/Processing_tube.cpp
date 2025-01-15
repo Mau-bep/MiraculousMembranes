18135eba5eb81acb88d9c24f840d659e1181cd3a
Mau <v.mauriciorojas@gmail.com>
2025-01-15 12:01:26 +0100

    Update Processing_tube.cpp

--- projects/geometric-flow/src/Processing_tube.cpp
+++ projects/geometric-flow/src/Processing_tube.cpp
@@ -612,7 +612,7 @@ int main(int argc, char** argv) {
                 filename = first_dir + "Tube_radius.txt" ;
                 Coverage_final =std::ofstream(filename,std::ios_base::app); 
     
-                Coverage_final<< rad<<" "<< KB << " "<< E_I<<" "<< Tube_r << " " << Tube_r_2 <<" "<< Interaction_str << KA <<"\n";
+                Coverage_final<< rad<<" "<< KB << " "<< E_I<<" "<< Tube_r << " " << Tube_r_2 <<" "<< Interaction_str << " "<< KA <<" \n";
                 Coverage_final.close();
 
                 // R_dist.close()
