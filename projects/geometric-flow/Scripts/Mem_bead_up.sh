5b357d9fb89cb34bff1d934390be5c779bcc7fe9
Mau <v.mauriciorojas@gmail.com>
2025-01-21 16:40:17 +0100

    New wrapping batch

--- projects/geometric-flow/Scripts/Mem_bead_up.sh
+++ projects/geometric-flow/Scripts/Mem_bead_up.sh
@@ -9,11 +9,11 @@ set Nsim=1
 # set Init_cond=1
 # foreach v ( 1.0 )
 # foreach Init_cond ( 2 )
-foreach Strg ( 10 50 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000)
+foreach Strg ( `seq 90 10 180`)
 # foreach Strg ( 400.0 500.0 600.0 700.0 )
-foreach KA ( 0.05 )
-foreach radius ( 0.2 0.3 0.4 )
-foreach KB ( 10.0 20.0 30.0 )
+foreach KA ( 0.0 0.3 0.4 0.6 0.9 1.2 )
+foreach radius ( 0.5 )
+foreach KB ( 30.0 )
 #python3 Create_subjob.py ${v} ${c0} ${KA} ${KB}
 
 python3 Create_subjob_beads.py ${Strg} ${radius} ${KA} ${KB} ${Nsim}
