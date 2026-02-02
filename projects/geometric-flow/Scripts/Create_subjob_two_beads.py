54d98921470c7c5526348d65e3ad708ba0937ab3
Mau <v.mauriciorojas@gmail.com>
2026-02-02 15:04:53 +0100

    Update Create_subjob_two_beads.py

--- projects/geometric-flow/Scripts/Create_subjob_two_beads.py
+++ projects/geometric-flow/Scripts/Create_subjob_two_beads.py
@@ -149,87 +149,87 @@ Config_path, sim_path = Create_json_wrapping_two_outside(angle,outside1,outside2
 
 
 
-# os.makedirs('../Subjobs/',exist_ok=True)
-# os.makedirs('../Outputs/',exist_ok=True)
+os.makedirs('../Subjobs/',exist_ok=True)
+os.makedirs('../Outputs/',exist_ok=True)
 
 
 
-# # Config_path, sim_path = Create_json_wrapping_two(KA,KB,radius,Strength,angle)
-# # Hopefully this works
-# Config_path, sim_path = Create_json_wrapping_two_outside(angle,outside1,outside2)
+# Config_path, sim_path = Create_json_wrapping_two(KA,KB,radius,Strength,angle)
+# Hopefully this works
+Config_path, sim_path = Create_json_wrapping_two_outside(angle,outside1,outside2)
 
 
-# # def main():
-# Output_name = 'output_two_beads_without_spring_theta_{}_{}_{}.output'.format(angle,location[outside1],location[outside2])
-# Output_path = '../Outputs/'+Output_name
+# def main():
+Output_name = 'output_two_beads_without_spring_theta_{}_{}_{}.output'.format(angle,location[outside1],location[outside2])
+Output_path = '../Outputs/'+Output_name
 
-# f=open('../Subjobs/subjob_serial_two_beads_without_spring_theta_{}_{}_{}'.format(angle,location[outside1],location[outside2]),'w')
+f=open('../Subjobs/subjob_serial_two_beads_without_spring_theta_{}_{}_{}'.format(angle,location[outside1],location[outside2]),'w')
 
-# f.write('#!/bin/bash \n')
-# f.write('# \n')
+f.write('#!/bin/bash \n')
+f.write('# \n')
 
-# f.write('#SBATCH --job-name=Wrap\n')
-# f.write('#SBATCH --output={}'.format(Output_path))
-# f.write('\n#\n')
+f.write('#SBATCH --job-name=Wrap\n')
+f.write('#SBATCH --output={}'.format(Output_path))
+f.write('\n#\n')
 
-# # f.write('module load boost\n')
+# f.write('module load boost\n')
 
-# f.write('#number of CPUs to be used\n')
-# f.write('#SBATCH --ntasks=1\n')
-# f.write('#Define the number of hours the job should run. \n')
-# f.write('#Maximum runtime is limited to 10 days, ie. 240 hours\n')
-# f.write('#SBATCH --time=10:01:20\n')
+f.write('#number of CPUs to be used\n')
+f.write('#SBATCH --ntasks=1\n')
+f.write('#Define the number of hours the job should run. \n')
+f.write('#Maximum runtime is limited to 10 days, ie. 240 hours\n')
+f.write('#SBATCH --time=10:01:20\n')
 
-# f.write('#\n')
-# f.write('#Define the amount of system RAM used by your job in GigaBytes\n')
-# f.write('#SBATCH --mem=5G\n')
-# f.write('#\n')
+f.write('#\n')
+f.write('#Define the amount of system RAM used by your job in GigaBytes\n')
+f.write('#SBATCH --mem=5G\n')
+f.write('#\n')
 
-# #f.write('#Send emails when a job starts, it is finished or it exits\n')
-# #f.write('#SBATCH --mail-user=mrojasve@ist.ac.at\n')
-# #f.write('#SBATCH --mail-type=ALL\n')
-# #f.write('#\n')
-
-
-# f.write('#SBATCH --no-requeue\n')
-# f.write('#\n')
+#f.write('#Send emails when a job starts, it is finished or it exits\n')
+#f.write('#SBATCH --mail-user=mrojasve@ist.ac.at\n')
+#f.write('#SBATCH --mail-type=ALL\n')
+#f.write('#\n')
 
 
-# f.write('\n')
-# f.write('#Do not export the local environment to the compute nodes\n')
-# f.write('#SBATCH --export=NONE\n')
-# f.write('\n')
-# f.write('#SBATCH --error=%x_%j.err \n')
-# f.write('unset SLURM_EXPORT_ENV\n')
-# f.write('#for single-CPU jobs make sure that they use a single thread\n')
-# f.write('export OMP_NUM_THREADS=1\n')
-# f.write('#SBATCH --nodes=1\n')
-# f.write('#SBATCH --cpus-per-task=1\n')
-
-# f.write('\n')
+f.write('#SBATCH --no-requeue\n')
+f.write('#\n')
 
 
-# # f.write('source /nfs/scistore16/wojtgrp/mrojasve/.bashrc\n')
-# f.write('export PATH="/nfs/scistore16/wojtgrp/mrojasve/.local/bin:$PATH"\n')
-# f.write('echo $PATH\n')
+f.write('\n')
+f.write('#Do not export the local environment to the compute nodes\n')
+f.write('#SBATCH --export=NONE\n')
+f.write('\n')
+f.write('#SBATCH --error=%x_%j.err \n')
+f.write('unset SLURM_EXPORT_ENV\n')
+f.write('#for single-CPU jobs make sure that they use a single thread\n')
+f.write('export OMP_NUM_THREADS=1\n')
+f.write('#SBATCH --nodes=1\n')
+f.write('#SBATCH --cpus-per-task=1\n')
+
+f.write('\n')
 
 
-# f.write('module load conda\n')
-# f.write('conda activate mir_membranes\n')
+# f.write('source /nfs/scistore16/wojtgrp/mrojasve/.bashrc\n')
+f.write('export PATH="/nfs/scistore16/wojtgrp/mrojasve/.local/bin:$PATH"\n')
+f.write('echo $PATH\n')
 
-# f.write('pwd\n')
 
-# f.write('date\n')
-# f.write('srun time -v ../build/bin/main_cluster {} {}\n'.format(Config_path,Nsim))
-# f.write('date\n')
-# #  Here we can tell the script to move the output file
+f.write('module load conda\n')
+f.write('conda activate mir_membranes\n')
 
-# f.write('cp {}.output {}/{}.txt \n'.format(Output_path,sim_path,Output_name) )
-# # I need to acces the data in the config file.
+f.write('pwd\n')
 
-# f.write('\n')
-# f.write('#sacct --format="JobID, State, AllocGRES, AllocNodes, CPUTime, ReqMem, MaxRSS, AveRSS, Elapsed" --units=G | head -n 1\n')
-# f.write('#sacct --format="JobID, State, AllocGRES, AllocNodes, CPUTime, ReqMem, MaxRSS, AveRSS, Elapsed" --units=G | tail -n 1\n')
-# f.write('\n')
+f.write('date\n')
+f.write('srun time -v ../build/bin/main_cluster {} {}\n'.format(Config_path,Nsim))
+f.write('date\n')
+#  Here we can tell the script to move the output file
 
-# f.close()
+f.write('cp {}.output {}/{}.txt \n'.format(Output_path,sim_path,Output_name) )
+# I need to acces the data in the config file.
+
+f.write('\n')
+f.write('#sacct --format="JobID, State, AllocGRES, AllocNodes, CPUTime, ReqMem, MaxRSS, AveRSS, Elapsed" --units=G | head -n 1\n')
+f.write('#sacct --format="JobID, State, AllocGRES, AllocNodes, CPUTime, ReqMem, MaxRSS, AveRSS, Elapsed" --units=G | tail -n 1\n')
+f.write('\n')
+
+f.close()
