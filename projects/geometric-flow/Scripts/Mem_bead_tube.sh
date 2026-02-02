6f2db10936f6d06b0b4adc464fbc5e2e73532e94
Mau <v.mauriciorojas@gmail.com>
2026-02-02 14:38:41 +0100

    Coordinating with the cluster

--- projects/geometric-flow/Scripts/Mem_bead_tube.sh
+++ projects/geometric-flow/Scripts/Mem_bead_tube.sh
@@ -15,6 +15,7 @@
 # foreach Strg ( `seq 20 5 140`)
 # foreach Strg ( 225.0  )
 foreach theta ( 0.2 0.25 0.3 0.325 0.35 0.375 0.4 0.425 0.45 0.475 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 )
+# foreach theta( 0.19 )
 #python3 Create_subjob.py ${v} ${c0} ${KA} ${KB}
 
 # python3 Create_subjob_two_beads.py ${theta} ${Strg} ${radius} ${KA} ${KB} ${Nsim}
@@ -25,4 +26,4 @@ sbatch ../Subjobs/subjob_bead_tube_theta_${theta}_300
 
 
 end
-end
+
